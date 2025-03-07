0bdb27210bd417aca4a5d1aa3e0316b1a55cea44
kouki223 <kouki.1818194@gmail.com>
2025-03-08 04:15:01 +0900

    クリックイベントの実装

--- src/component/Task.jsx
+++ src/component/Task.jsx
@@ -1,7 +1,26 @@
-import { Checkbox, Flex, Text } from "@chakra-ui/react";
+import { Checkbox, Flex, Text, Input, Button } from "@chakra-ui/react";
 import { CloseIcon } from "@chakra-ui/icons";
+import { useState } from "react";
 
 const Task = (props) => {
+  const [isEdit, setIsEdit] = useState(false)
+  const [editName, setEditName] = useState(props.name)
+
+  const onClickEdit = () => {
+    console.log("編集ボタンをクリックしました")
+    setIsEdit(true)
+  }
+
+  const onClicSave = () => {
+    console.log("保存ボタンをクリックしました")
+    setIsEdit(false)
+  }
+
+  const onClickCancel = () => {
+    console.log("キャンセルボタンをクリックしました")
+    setIsEdit(false)
+  }
+
   return (
     <Flex mb="16px" justifyContent="space-between" alignItems="center">
       <Checkbox
@@ -12,8 +31,19 @@ const Task = (props) => {
           props.toggleIsDone(props.id, props.index);
         }}
       >
-        <Text>{props.name}</Text>
       </Checkbox>
+      {!isEdit ?
+      <>
+        <Text>{props.name}</Text>
+        <Button onClick={onClickEdit}>編集</Button>
+      </>
+      :
+      <Flex>
+        <Input value={editName} onChange={(e) => setEditName(e.target.value)} />
+        <Button onClick={onClicSave}>保存</Button>
+        <Button onClick={onClickCancel}>キャンセル</Button>
+      </Flex>
+      }
       <CloseIcon onClick={() => props.destroyTask(props.id)} />
     </Flex>
   );
